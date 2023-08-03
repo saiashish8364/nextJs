29f1dfe607e2407a1477770f9fc5f7f41e283458
saiashish8364 <polusaiashish97@gmail.com>
2023-08-03 20:34:53 +0530

    up4

--- next-app/pages/AboutUs/index.js
+++ next-app/pages/AboutUs/index.js
@@ -1,4 +1,20 @@
+import Link from "next/link";
 function AboutUs() {
-  return <h1>this is AboutUs page</h1>;
+  return (
+    <>
+      <h1>this is AboutUs page</h1>
+      <ul>
+        <li>
+          <Link href="/AboutUs/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/AboutUs/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/AboutUs/3">Suresh</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 export default AboutUs;
